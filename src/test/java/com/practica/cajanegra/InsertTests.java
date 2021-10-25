f4c383d9c37ce88ff4f4a9e5b5b70458fe29b7e2
christianlutrilla <c.lamor@alumnos.upm.es>
2021-10-25 19:25:12 +0200

    Tests de addAtPos (Falta al excepción)

--- src/test/java/com/practica/cajanegra/InsertTests.java
+++ src/test/java/com/practica/cajanegra/InsertTests.java
@@ -13,7 +13,6 @@ public class InsertTests {
         this.miLista = new SingleLinkedListImpl<String>("A","B","C");
     }
 
-
     @ParameterizedTest(name="Add Last {0} in list")
     @ValueSource(strings= {"A", "M", "Z", "@","["})
     public void addLast(String s) {
@@ -24,5 +23,35 @@ public class InsertTests {
             assertEquals("[A, B, C, " + s + "]", this.miLista.toString());
     }
 
+    @ParameterizedTest(name="Add value {0} at Pos 2 ")
+    @ValueSource(strings={"@", "A", "M", "Z", "["})
+    public void addAtPosTestString(String s){
+        this.miLista.addAtPos(s, 2);
+        if((s.charAt(s.length()-1)=='@') || (s.charAt(s.length()-1)=='['))
+            assertNotEquals("[A, " + s + ", B, C]", this.miLista.toString());
+        else
+            assertEquals("[A, "+ s +", B, C]", this.miLista.toString());
+    }
+
+    /*
+    @ParameterizedTest(name="Add value M at Pos {0}")
+    @ValueSource(ints={0, 1, 2, 3})
+    public void addAtPosTestPosition(int p) {
+        if(p <= 0)
+            assertThrows(java.lang.IllegalArgumentException);
+        else if(p == 1) {
+            this.miLista.addAtPos("M", p);
+            assertEquals("[M, A, B, C]", this.miLista.toString());
+        }
+        else if(p == 2) {
+            this.miLista.addAtPos("M", p);
+            assertEquals("[A, M, B, C]", this.miLista.toString());
+        }
+        else {
+            this.miLista.addAtPos("M", p);
+            assertEquals("[A, B, M, C]", this.miLista.toString());
+        }
+    }
+    */
 }
 
