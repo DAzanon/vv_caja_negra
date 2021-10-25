b48e1bf9cd72ca348ee2b35d44fcbd5592454cec
D.Azanon <d.azanon@alumnos.upm.es>
2021-10-25 17:47:10 +0200

    AddLast acabado

--- src/test/java/com/practica/cajanegra/InsertTests.java
+++ src/test/java/com/practica/cajanegra/InsertTests.java
@@ -1,12 +1,10 @@
 package com.practica.cajanegra;
-
 import com.cajanegra.SingleLinkedListImpl;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.ValueSource;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class InsertTests {
     private SingleLinkedListImpl<String> miLista;
@@ -14,11 +12,16 @@ public class InsertTests {
     public void setUp() {
         this.miLista = new SingleLinkedListImpl<String>("A","B","C");
     }
+
+
     @ParameterizedTest(name="Add Last {0} in list")
-    @ValueSource(strings= {"@", "A", "B", "M", "Y", "Z", "["})
+    @ValueSource(strings= {"A", "M", "Z", "@","["})
     public void addLast(String s) {
         this.miLista.addLast(s);
-        assertEquals("[A, B, C, " + s + "]", this.miLista.toString());
+        if((s.charAt(s.length()-1)=='@') || (s.charAt(s.length()-1)=='['))
+            assertNotEquals("[A, B, C, " + s + "]", this.miLista.toString());
+        else
+            assertEquals("[A, B, C, " + s + "]", this.miLista.toString());
     }
 
 }
