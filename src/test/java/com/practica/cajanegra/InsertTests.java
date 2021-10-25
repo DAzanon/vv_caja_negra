ed7601ea063059fb168a75ae567d9fbcac818fcb
DAzanon <d.azanon@alumnos.upm.es>
2021-10-25 11:39:58 +0200

    Primeros tests en AddLast, en InsertTests

--- src/test/java/com/practica/cajanegra/InsertTests.java
+++ src/test/java/com/practica/cajanegra/InsertTests.java
@@ -1,4 +1,25 @@
 package com.practica.cajanegra;
 
+import com.cajanegra.SingleLinkedListImpl;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class InsertTests {
+    private SingleLinkedListImpl<String> miLista;
+    @BeforeEach
+    public void setUp() {
+        this.miLista = new SingleLinkedListImpl<String>("A","B","C");
+    }
+    @ParameterizedTest(name="Add Last {0} in list")
+    @ValueSource(strings= {"@", "A", "B", "M", "Y", "Z", "["})
+    public void addLast(String s) {
+        this.miLista.addLast(s);
+        assertEquals("[A, B, C, " + s + "]", this.miLista.toString());
+    }
+
 }
+
